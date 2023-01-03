82a11d11346d8c498b9738f1c508afe64d531b4d
riotrequest <121694664+riotrequest@users.noreply.github.com>
2023-01-03 11:53:22 -0500

    Update openai.py

--- openai.py
+++ openai.py
@@ -1,34 +1,26 @@
-const axios = require("axios");
+import requests
 
-async function sendRequest() {
-  // Set the API endpoint URL
-  const endpointUrl = "https://api.openai.com/v1/models/<model_name>/versions/<model_version>/prompts/<prompt_id>";
+# Set the API endpoint URL
+endpoint_url = "https://api.openai.com/v1/models/<model_name>/versions/<model_version>/prompts/<prompt_id>"
 
-  // Set the API key
-  const apiKey = "<your_api_key>";
+# Set the API key
+api_key = "<your_api_key>"
 
-  // Set the request headers
-  const headers = {
-    "Content-Type": "application/json",
-    "Authorization": `Bearer ${apiKey}`
-  };
-
-  // Set the request body
-  const data = {
-    "model": "<model_name>",
-    "prompt": "<prompt_text>",
-    "temperature": 0.5
-  };
-
-  try {
-    // Make the request
-    const response = await axios.post(endpointUrl, data, {headers});
-
-    // Print the response
-    console.log(response.data);
-  } catch (error) {
-    console.error(error);
-  }
+# Set the request headers
+headers = {
+  "Content-Type": "application/json",
+  "Authorization": f"Bearer {api_key}"
 }
 
-sendRequest();
+# Set the request body
+data = {
+  "model": "<model_name>",
+  "prompt": "<prompt_text>",
+  "temperature": 0.5
+}
+
+# Make the request
+response = requests.post(endpoint_url, json=data, headers=headers)
+
+# Print the response
+print(response.json())
